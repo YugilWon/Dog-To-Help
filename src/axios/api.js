245446029f7a4ec29f05b2e769cf451793150957
YuGil <yugil3150@naver.com>
2023-07-12 09:17:13 +0900

    dd

--- src/axios/api.js
+++ src/axios/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
-  timeout: 1000,
+  timeout: 10000,
 });
 
 const addPost = async (newPost) => {
